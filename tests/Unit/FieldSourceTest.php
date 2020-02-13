bda5d886f7030039c52b3374f5aa40ccc8c31599
Matt Koch <kochm@mskcc.org>
2020-02-13 15:05:48 -0500

    use parameter binding instead of raw SQL

--- tests/Unit/FieldSourceTest.php
+++ tests/Unit/FieldSourceTest.php
@@ -8,30 +8,17 @@ use Tests\TestCase;
 class FieldSourceTest extends TestCase
 {
     /** @test */
-    public function it_can_substitute_an_integer_identifier_in_a_query()
+    public function it_can_return_a_sql_query()
     {
         $fieldSource = factory(FieldSource::class)->make([
-            'query' => "SELECT gender from PATIENT where id = id"
+            'query' => "SELECT gender from PATIENT where id = ?"
         ]);
 
         $this->assertEquals(
-            "SELECT gender from PATIENT where id = 1",
-            $fieldSource->getQueryFor(1)
+            "SELECT gender from PATIENT where id = ?",
+            $fieldSource->getQuery()
         );
 
-    }
-
-    /** @test */
-    public function it_can_substitute_a_string_identifier_in_a_query()
-    {
-        $fieldSource = factory(FieldSource::class)->make([
-            'query' => "SELECT gender from PATIENT where id = sid"
-        ]);
-
-        $this->assertEquals(
-            "SELECT gender from PATIENT where id = '12345678'",
-            $fieldSource->getQueryFor("12345678")
-        );
     }
 
 
